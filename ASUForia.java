be8aef0310568cbdbf10c8dc4e985b37dc7ce226
npkulka3 <31780855+npkulka3@users.noreply.github.com>
2017-12-04 19:36:12 -0700

    Added Comments

--- ASUForia.java
+++ ASUForia.java
@@ -1,5 +1,10 @@
 package com.example.neera.once_more;
-
+/*
+ASUForia class is declared here. The interface PoseListener() is defined alongwith onPose().
+onImageAvailable() is used to call nativePoseEstimation() in native-lib.cpp
+TODO: in endEstimation() : 1. release Camera for other use
+                           2. stop all Background thread and process
+ */
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -7,10 +12,8 @@ import android.view.Surface;
 
 import static com.example.neera.once_more.MainActivity.bmpIn;
 
-/**
- * Created by neera on 12/4/2017.
- */
 
+//declare the class
 public  class ASUForia {
     static {
         System.loadLibrary("native-lib");
@@ -18,19 +21,24 @@ public  class ASUForia {
 //created constructor
     ASUForia(PoseListener lister_arg, Bitmap ref_img, Surface cam_Surface)
     {
-        myListener = lister_arg;
+        new_Listener = lister_arg;
     }
 
-    private PoseListener myListener;
+    private PoseListener new_Listener;
 
     interface PoseListener {
+        //setup onPose() function
          void onPose(Image cam_Frame,float[] rvec,float[] tvec );
     }
 
    private void endEstimation(){
 
    }
-
+/*
+Receive Camera Frame(as a byte array) from Camera Preview.
+Converting byte array to int array for camera Image and reference Image and calling nativePoseEstimation() in C++
+nativePoseEstimation() returns an int array of output image pixels
+ */
     public Bitmap onImageAvailable(byte[] imageBytes){
         Bitmap bmp = BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.length);
         Bitmap out=bmp.copy(bmp.getConfig(),true);
@@ -45,16 +53,19 @@ public  class ASUForia {
         int h_1 = bmpIn.getHeight();
         int str_1 = w_1;
         int [] pixels_ref = new int [w_1*h_1];
+        //converting reference image to int array
         bmpIn.getPixels(pixels_ref,offset,str_1,0,0,w_1,h_1);
         //bmpIn.recycle();
+        //convertig Camera Image to int array
         bmp.getPixels(pixels,offset,stride,0,0,width,height);
         bmp.recycle();
 
-        // byte [] image_out = new byte[imageBytes.length];
+        // calling nativePoseEstimation in C++
         pixels_new=nativePoseEstimation(pixels,pixels_ref,width,height,w_1,h_1);
-        //Bitmap bmp1 =BitmapFactory.decodeByteArray(image_out,0,image_out.length);
+
+        //converting output aray of pixels to Bitmap and return the bitmap
         out.setPixels(pixels_new,offset,stride,0,0,width,height);
-        //myListener.onPose();
+
         return out;
 
     }
