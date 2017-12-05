58f27811a01c739effa3a3d1ff1dfed7c41e80d0
npkulka3 <31780855+npkulka3@users.noreply.github.com>
2017-12-04 17:31:47 -0700

    Update AutoFitTextureView.java

--- AutoFitTextureView.java
+++ AutoFitTextureView.java
@@ -1,27 +1,21 @@
 /*
- * Copyright 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+This file implements a Camera View on the screen of the device.
+Parts of this code is referred from https://developer.android.com/samples/Camera2Raw/src/com.example.android.camera2raw/Came
+ra2RawFragment.html
+The Aspect Ratio of the camera screen and setting the texture view is implemeted in this file
+
  */
 
-package com.example.neera.assign_2_cpp;
+
+package com.example.neera.once_more;
 
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.TextureView;
 
 /**
- * A {@link TextureView} that can be adjusted to a specified aspect ratio.
+ * A TextureView that can be adjusted to a specified aspect ratio.
  */
 public class AutoFitTextureView extends TextureView {
 
@@ -42,11 +36,7 @@ public class AutoFitTextureView extends TextureView {
 
     /**
      * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
-     * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
-     * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
-     *
-     * @param width  Relative horizontal size
-     * @param height Relative vertical size
+     * calculated from the parameters.
      */
     public void setAspectRatio(int width, int height) {
         if (width < 0 || height < 0) {
